4866bf9f436a43961699580e79273eaa5585ac69
Amir Yalamov <amir.yalamov@gmail.com>
2020-04-14 13:35:25 -0400

    init commit with code files

---
+++ project/ping.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <netinet/ip_icmp.h>
+#include <time.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <time.h>
+#include <iostream>
+
+int main (int argc, char *argv[]) {
+
+    return 0;
+}
